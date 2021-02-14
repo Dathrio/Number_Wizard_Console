873dbf8717a0a11bea80adf453fb4e6e35e6ac12
Kevin Griffith <kevingriffith.kg@gmail.com>
2021-02-13 19:07:01 -0800

    Added some comments and optimized console responses

--- Assets/NumberWizard.cs
+++ Assets/NumberWizard.cs
@@ -5,44 +5,47 @@ using UnityEngine;
 
 public class NumberWizard : MonoBehaviour
 {
+    // initialize key variables
     int max = 1000;
     int min = 1;
     int guess = 500;
     // Start is called before the first frame update
     void Start()
     {
+        //welcome script for the player
         Debug.Log("Welcome to Number Wizard");
         Debug.Log("Think of a number...");
         Debug.Log("The Lowest you can pick is: " + min);
         Debug.Log("The Highest you can pick is: " + max);
         Debug.Log("Is your number higher or lower than " + guess + "?");
         Debug.Log("Push Up = Higher, Push Down = Lower, Push Enter = Correct");
+        //fixes bug where we can never guess 1,000
         max = max + 1;
     }
 
     // Update is called once per frame
     void Update()
     {
-        //Detect when the up arrow key is pressed down
+        //Detect when the up arrow key is pressed down and update variables
         if (Input.GetKeyDown(KeyCode.UpArrow))
         {
-            Debug.Log("Up Arrow key was pressed.");
             min = guess;
             guess = (min + max) / 2;
-            Debug.Log(guess);
+            Debug.Log("I was just warming up... My real gues is: " + guess);
         }
 
+        //Detect when the down arrow key is pressed down and update variables
         else if (Input.GetKeyDown(KeyCode.DownArrow))
         {
-            Debug.Log("Down Arrow key was pressed.");
             max = guess;
             guess = (min + max) / 2;
-            Debug.Log(guess);
+            Debug.Log("You're a sneaky one! But your number is: " + guess);
         }
 
+        //Computer won | restart the game
         else if (Input.GetKeyDown(KeyCode.Return))
         {
-            Debug.Log("Enter Key was pressed.");
+            Debug.Log("I told you I could guess it.");
         }
     }
 }
